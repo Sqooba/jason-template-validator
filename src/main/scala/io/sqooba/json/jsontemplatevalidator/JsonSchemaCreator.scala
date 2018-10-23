c221ce91985714f023a49c1a3071ed4e931289a5
Pietari Kettunen <pietari@sqooba.io>
2018-10-23 09:43:56 +0200

    schema gen

---
+++ src/main/scala/io/sqooba/json/jsontemplatevalidator/JsonSchemaCreator.scala
@@ -0,0 +1,18 @@
+package io.sqooba.json.jsontemplatevalidator
+
+object JsonSchemaCreator extends App {
+
+  println(s"perse: ${args.length}")
+
+}
+
+
+class JsonSchemaCreator() {
+
+
+  def createSchemaFor(jsonFile: String): String = {
+
+    ""
+  }
+
+}
