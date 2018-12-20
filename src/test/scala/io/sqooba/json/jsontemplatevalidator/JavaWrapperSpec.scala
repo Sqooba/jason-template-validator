50084cecac8706c882d8120c1732e6d4f448c95d
Pietari Kettunen <pietari@sqooba.io>
2018-12-20 13:41:52 +0100

    accessor for java method

--- src/test/scala/io/sqooba/json/jsontemplatevalidator/JavaWrapperSpec.scala
+++ src/test/scala/io/sqooba/json/jsontemplatevalidator/JavaWrapperSpec.scala
@@ -17,4 +17,15 @@ class JavaWrapperSpec extends FlatSpec with Matchers {
     res2 shouldBe a [java.lang.Boolean]
     res2 shouldBe true
   }
+
+  "JsonValidatorAsJava" should "procudes set of errors" in {
+    val validator = JsonValidator.forTemplateJson(json2).asJava()
+    val res1 = validator.validateJson(json1)
+    res1 shouldBe a [java.lang.Boolean]
+    res1 shouldBe false
+
+    val errors = validator.getLatestErrors()
+    errors.size() shouldBe 1
+    errors.contains("other") shouldBe true
+  }
 }
