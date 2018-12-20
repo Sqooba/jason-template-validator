3334a815e507ee5012daf39133321903b5a2e28d
Pietari Kettunen <pietari@sqooba.io>
2018-12-20 13:38:56 +0100

    add java access for errors

--- src/main/scala/io/sqooba/json/jsontemplatevalidator/JsonValidatorForJava.scala
+++ src/main/scala/io/sqooba/json/jsontemplatevalidator/JsonValidatorForJava.scala
@@ -1,10 +1,17 @@
 package io.sqooba.json.jsontemplatevalidator
 
+import java.util
+
 import com.fasterxml.jackson.databind.JsonNode
 
 class JsonValidatorForJava(validator: JsonValidator) {
 
   def validateJson(json: JsonNode): java.lang.Boolean = validator.validateJson(json)
   def validateJson(json: String): java.lang.Boolean = validator.validateJson(json)
+  def getLatestErrors(): java.util.Set[String] = {
+    val set: java.util.Set[String] = new util.HashSet[String]()
+    validator.getLatestErrors().foreach(erField => set.add(erField))
+    set
+  }
 
 }
